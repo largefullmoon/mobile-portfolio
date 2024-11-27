3926021829217d94fea689096e4587cadda0db6b
largefullmoon <largefullmoon@gmail.com>
2024-11-27 04:23:29 +0200

    update with phone images

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {
 } from "./components";
 import { useEffect } from "react";
 import { config } from "./constants/config";
-
+import Phone from './assets/phone.png'
 const App = () => {
   useEffect(() => {
     if (document.title !== config.html.title) {
@@ -23,22 +23,30 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <div className="bg-primary relative z-0">
-        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
-          <Navbar />
-          <Hero />
+      <div>
+        <div className="fixed right-32 top-0 z-10 opacity-80">
+          <img src={Phone} className="h-screen"/>
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+        <div className="fixed left-20 top-0 z-10 opacity-80 rotate-90">
+          <img src={Phone} className="h-screen"/>
+        </div>
+        <div className="bg-primary relative z-0">
+          <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
       </div>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 };
 
